d075a33063a98b4ffeb51df0e74aabc7277f42d7
Tuyet Lan <tuyetlannguyenthi.wor@gmail.com>
2026-03-19 16:15:52 +0700

    ASP 19/3

--- DBConnect/Controllers/WeatherForecastController.cs
+++ DBConnect/Controllers/WeatherForecastController.cs
@@ -4,6 +4,7 @@ namespace DBConnect.Controllers
 {
     [ApiController]
     [Route("[controller]")]
+    //
     public class WeatherForecastController : ControllerBase
     {
         private static readonly string[] Summaries = new[]
